3a369999d9428f0e2494789a12c0d7dd7cdca5dd
97anna1997 <anna19779697@gmail.com>
2024-06-29 15:59:10 +0400

    init project

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+import Login from './login';
+import Card from './components/card';
+import Buttn from './components/button'
+import Button from './components/button';
+
+function App() {
+    return (
+        < div >
+            <h2>App</h2>
+            <Card />
+            <hr />
+            <Button />
+        </div>
+        /* <Login
+            title="Login Form"
+            list={['free doamin']}
+        />
+        < hr />
+
+        <Login title="Register Form"
+            list={[
+                'free doamin',
+                'free hosting',
+                'SSL Key',
+            ]} */
+        // />
+        // </div >
+    )
+}
+
+export default App;
+
+
+
